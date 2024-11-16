af0a36d40b624c139ffdacbb921ad950b6d6bfeb
c1rcle <100463851+c1rcle-xy@users.noreply.github.com>
2024-11-16 20:46:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-
+pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
+#换源
+apt update && apt upgrade
+pkg install python libxml2 libxslt pkg-config
+pip install cython wheel
+CFLAGS="-Wno-error=incompatible-function-pointer-types -O0" pip install lxml
