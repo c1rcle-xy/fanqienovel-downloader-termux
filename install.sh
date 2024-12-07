07bbf7eb5b69120a151848a62c2cc96e13d4c12f
c1rcle <100463851+c1rcle-xy@users.noreply.github.com>
2024-12-07 19:08:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
-pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
+termux-setup-storage
+echo -e "\e[4m请赋予termux存储权限，如已赋予请忽视"
+sed -i 's@^ $deb.*stable main$ $@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list
+apt update && apt upgrade
+#pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
 #换源
 apt update && apt upgrade
 pkg install python libxml2 libxslt pkg-config
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+#pip换源
+#pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 pip install cython wheel
 CFLAGS="-Wno-error=incompatible-function-pointer-types -O0" pip install lxml
+#安装最难装的lxml库（这个方法我也不记得在哪找的了）
+pip install requests lxml ebooklib tqdm beautifulsoup4
+#其他库
+echo "正在从 GitHub 下载项目文件，此处可能需要科学上网，快了快了←_←"
+curl -o FQToolBox.zip "https://codeload.github.com/ying-ck/fanqienovel-downloader/zip/refs/heads/main" || { echo "下载失败，请尝试使用科学上网(〒﹏〒)"; exit 1; }
+echo "正在解压ing..."
+unzip -o -d ~/storage/downloads fanqienovel-downloader-main.zip
+cd ~/storage/downloads/fanqienovel-downloader-main
+python ./src/ref_main.py
