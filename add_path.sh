428621b7156b564808d3afb632dcdc3e6abd704a
c1rcle <100463851+c1rcle-xy@users.noreply.github.com>
2024-12-07 20:19:16 +0800

    更新 add_path.sh

--- add_path.sh
+++ add_path.sh
@@ -1,1 +1,2 @@
-alias fq='cd ~/storage/downloads/fanqienovel-downloader-main/src && python ./ref_main.py'
+sed -i '$ a\alias fq="cd ~/storage/downloads/fanqienovel-downloader-main/src && python ./ref_main.py"' ~/.bashrc
+source ~/.bashrc
