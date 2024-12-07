266c6d55e987c557d55b76eb189dbd1b320e07bf
c1rcle <100463851+c1rcle-xy@users.noreply.github.com>
2024-12-07 20:25:03 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,6 @@ unzip -o -d ~/storage/downloads fanqienovel-downloader-main.zip
 cd ~/storage/downloads/fanqienovel-downloader-main/src
 sed -i '5d' ref_main.py
 #删tk库
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/c1rcle-xy/FQToolBox-termux/refs/heads/main/fqtoolbox.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/c1rcle-xy//refs/heads/main/add_path.sh)"
 #环境变量
 python ref_main.py
