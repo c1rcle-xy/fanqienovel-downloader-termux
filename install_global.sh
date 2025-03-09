36cfee4daef860f8484a3f6666bd87c758a02a55
c1rcle <100463851+c1rcle-xy@users.noreply.github.com>
2025-03-09 09:50:35 +0800

    更新 install_global.sh

--- install_global.sh
+++ install_global.sh
@@ -5,7 +5,7 @@ pkg install python libxml2 libxslt pkg-config
 pip install cython wheel
 CFLAGS="-Wno-error=incompatible-function-pointer-types -O0" pip install lxml
 #安装最难装的lxml库（这个方法我也不记得在哪找的了）
-pip install requests lxml ebooklib tqdm beautifulsoup4 openssl -y
+pip install requests lxml ebooklib tqdm beautifulsoup4 openssl qrcode -y
 #其他库
 echo "正在从 GitHub 下载项目文件..."
 curl -o fanqienovel-downloader-main.zip "https://github.com/ying-ck/fanqienovel-downloader/archive/refs/heads/main.zip" || { echo "下载失败"; exit 1; }
